c124bcc97dd1a4898dc38252beb9e6bd144671f0
Abishek <abishek.50c@gmail.com>
2020-11-30 00:02:09 +0530

    implemented insert in AVLTree

--- AVLTree.java
+++ AVLTree.java
@@ -28,7 +28,99 @@ public class AVLTree extends BSTree {
     
     public AVLTree Insert(int address, int size, int key) 
     { 
-        return null;
+        AVLTree root = this.findRoot();
+        AVLTree parent = root;
+        AVLTree temp = new AVLTree(address,size,key);
+        AVLTree current = parent.right;
+        while (current != null){
+            parent = current;
+            if (current.key>key){
+                current=current.left;
+            }
+            else if (current.key<key){
+                current=current.right;
+            }
+            else{
+                if (current.address>address){
+                    current=current.left;
+                }
+                else if (current.address<address){
+                    current=current.right;
+                }
+                else{
+                    if (current.size>size){
+                        current=current.left;
+                    }
+                    else if (current.size<size){
+                        current=current.right;
+                    }
+                    else{
+                        return null;
+                    }
+                }
+            }
+        }
+        if (parent == root){
+            parent.right = temp;
+        }
+        else if (parent.key>key){
+            parent.left = temp;
+        }
+        else if (parent.key<key){
+            parent.right = temp;
+        }
+        else{
+            if (parent.address>address){
+                parent.left = temp;
+            }
+            else if (parent.address<address){
+                parent.right = temp;
+            }
+            else{
+                if (parent.size>size){
+                    parent.left = temp;
+                }
+                else if (parent.size<size){
+                    parent.right = temp;
+                }
+                else{
+                    return null;
+                }    
+            }
+        }
+        temp.parent = parent;
+        AVLTree z = parent;
+        AVLTree y = z;
+        AVLTree x = z;
+        while(z.parent != null){
+            int balance = height(z.left) - height(z.right);
+            if (balance > 1){
+                if (y.left == x){
+                    rightRotate(z);
+                }
+                else{
+                    leftRotate(y);
+                    rightRotate(z);
+                }
+                return temp;
+            }
+            else if (balance < -1){
+                if (y.right == x){
+                    leftRotate(z);
+                }
+                else{
+                    rightRotate(y);
+                    leftRotate(z);
+                }
+                return temp;
+            }
+            z.height = Math.max(height(z.left),height(z.right)) + 1;
+            x=y;
+            y=z;
+            z = z.parent;
+            
+        }
+        return temp;
     }
 
     public boolean Delete(Dictionary e)
@@ -36,24 +128,150 @@ public class AVLTree extends BSTree {
         return false;
     }
         
-    public AVLTree Find(int k, boolean exact)
-    { 
-        return null;
-    }
+    // public AVLTree Find(int k, boolean exact)
+    // { 
+    //     return null;
+    // }
 
-    public AVLTree getFirst()
-    { 
-        return null;
-    }
+    // public AVLTree getFirst()
+    // { 
+    //     return null;
+    // }
 
-    public AVLTree getNext()
-    {
-        return null;
-    }
+    // public AVLTree getNext()
+    // {
+    //     return null;
+    // }
 
     public boolean sanity()
     { 
         return false;
+    }
+
+    private AVLTree findRoot()
+    {
+        if (this.parent == null){
+            return this;
+        }
+        else{
+            return this.parent.findRoot();
+        }
+    }
+
+    private int height(AVLTree node){
+        if (node == null){
+            return -1;
+        }
+        else{
+            return node.height;
+        }
+    }
+
+    private void rightRotate(AVLTree node){
+        AVLTree y = node.left;
+        AVLTree T3 = y.right;
+        node.left = T3;
+        if (T3 != null){
+            T3.parent = node;
+        }
+        y.right = node;
+        y.parent = node.parent;
+        node.parent = y;
+        if (y.parent.parent == null || y.parent.isLessthan(node)){
+            y.parent.right = y;
+        }
+        else{
+            y.parent.left = y;
+        }
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        y.height = Math.max(height(y.left),height(y.right)) + 1;
+
+    }
+
+    private void leftRotate(AVLTree node){
+        AVLTree y = node.right;
+        AVLTree T3 = y.left;
+        node.right = T3;
+        if (T3 != null){
+            T3.parent = node;
+        }
+        y.left = node;
+        y.parent = node.parent;
+        node.parent = y;
+        if (y.parent.parent == null || y.parent.isLessthan(node)){
+            y.parent.right = y;
+        }
+        else{
+            y.parent.left = y;
+        }
+        node.height = Math.max(height(node.left),height(node.right)) + 1;
+        y.height = Math.max(height(y.left),height(y.right)) + 1;
+
+    }
+    
+
+    private boolean isLessthan(AVLTree child){
+        int key = child.key;
+        int address = child.address;
+        int size = child.size;
+        AVLTree current = this;
+        if (current.key>key){
+            return false;
+        }
+        else if (current.key<key){
+            return true;
+        }
+        else{
+            if (current.address>address){
+                return false;
+            }
+            else if (current.address<address){
+                return true;
+            }
+            else{
+                if (current.size>size){
+                    return false;
+                }
+                else if (current.size<size){
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    }
+
+    private boolean isGreaterthan(AVLTree child){
+        int key = child.key;
+        int address = child.address;
+        int size = child.size;
+        AVLTree current = this;
+        if (current.key>key){
+            return true;
+        }
+        else if (current.key<key){
+            return false;
+        }
+        else{
+            if (current.address>address){
+                return true;
+            }
+            else if (current.address<address){
+                return false;
+            }
+            else{
+                if (current.size>size){
+                    return true;
+                }
+                else if (current.size<size){
+                    return false;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
     }
 }
 
